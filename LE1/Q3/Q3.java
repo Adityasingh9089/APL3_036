a8c1d7ffe09306be30515255e742238173140e10
221B036 <221B036@juet.co.in>
2025-01-27 14:23:59 +0530

    Lab1

---
+++ LE1/Q3/Q3.java
@@ -0,0 +1,23 @@
+class Mother{
+    static int show(int a,int b){
+        System.out.println("Hello World");
+        return 0;
+    }
+}
+class Child extends Mother{
+    static int show(int a,int b){
+        System.out.println("Hello JUET");
+        return 0;
+    }
+}
+public class Application
+{
+	public static void main(String[] args) {
+		Mother m= new Mother ( );
+        m.show(7,8); // show of Mother is called
+        Child ch=new Child ( ); 
+        ch. show (7,8); // show ( ) inherited in Child from Mother is called
+        Mother m1=new Child ( );
+        m1.show(7,8);
+	}
+}
